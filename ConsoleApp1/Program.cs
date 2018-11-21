b53f9b35974508dca4e4cdb7bdca6f5582c71cc2
Artsiom Artsiomenka <AArtsiomenka@gomel.iba.by>
2018-11-21 13:32:36 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Str = "my string";
+            string Str1 = null;
+            TextTransformer text = new TextTransformer();
+            text.Add(Str);
+            text.Add(Str1);
+
+            foreach (var s in text)
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/TextTransformer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class TextTransformer
+    {
+        public List<string> Str = new List<string>();
+
+        public TextTransformer(string str)
+        {
+            Str.Add(str);
+        }
+
+        public TextTransformer() { }
+
+        public void Add(string item)
+        {
+            Str.Add(item);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            foreach (string s in Str)
+            {
+                if (s != null)
+                    yield return s.ToUpper();
+                else
+                    yield return "Empty string!";
+            }
+        }
+
+    }
+}
